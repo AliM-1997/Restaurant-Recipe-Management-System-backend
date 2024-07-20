17916c27216eceece64de98aea499bc204b9138d
AliM-1997 <160022725+AliM-1997@users.noreply.github.com>
2024-07-20 20:47:55 +0300

    Recipes (#2)
    * adding the delete recipes
    
    * adding the add_recipe file and updating the connection file
    
    * adding the add_recipe php file
    
    * adding the read all recipes
    
    * adding the update recipe

---
+++ recipes/read_all_recipes.php
@@ -0,0 +1,45 @@
+<?php
+
+
+require '../connection.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $resturant_name = $_POST["name"];
+    
+    
+    $stm = $conn->prepare("select * from resturants where name = ?");
+    $stm->bind_param("s", $resturant_name);
+    $stm->execute();
+    $result = $stm->get_result();
+    
+    if ($result->num_rows > 0) {
+        $stm = $conn->prepare("
+        select rs.name as resturant_name,r.id as recipe_id, r.name as recipe_name,r.details as recipe_details
+        from recipes r 
+        join resturants rs on rs.id=r.resturant_id
+        where rs.name=?");
+
+        $stm->bind_param("s", $resturant_name);
+        try {
+            $stm->execute();
+            $all_results = $stm->get_result();
+            $recipes = [];
+            if ($all_results->num_rows > 0) {
+                while ($row = $all_results->fetch_assoc()) {
+                    $recipes[] = $row;
+                }
+                echo json_encode(["recipes" => $recipes]);
+            } else {
+                echo json_encode(["message" => "No recipes found"]);
+            }
+        } catch (Exception $e) {
+            echo json_encode(["message" => "Error occurred while fetching recipes"]);
+        }
+    } else {
+        echo json_encode(["message" => "No restaurant found"]);
+    }
+
+} else {
+    echo json_encode(["message" => "Wrong request method"]);
+}
+?>
