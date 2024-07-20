e25a070d5dfd4056ba9bdb1df801cd28652d273b
AliM-1997 <aliimortada797@gmail.com>
2024-07-20 23:00:55 +0300

    inhancing the update by adding validation method

--- restaurants/update.php
+++ restaurants/update.php
@@ -7,6 +7,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $cuisine_type = $_POST["cuisine_type"];
     $id = $_POST["id"];
 
+    $stmt = $conn->prepare("select * from resturants where id =?");
+    $stmt->bind_param("i",$id);
+    $stmt->execute();
+    $results=$stmt->get_result();
+    if ($results->num_rows >0){
+        echo json_encode(["number of row"=>$results->num_rows]);
     $stmt = $conn->prepare("update resturants set name=?, location=?, cuisine_type=? where id=?;");
     $stmt->bind_param("sssi", $name, $location, $cuisine_type, $id);
     try {
@@ -15,6 +21,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } catch (Exception $e) {
         echo json_encode(["error" => $stmt->error, "status" => "failure"]);
     }
+}else{
+    echo json_encode(["message"=>"resturant not found"]);
+}
 } else {
     echo json_encode(["error" => "Wrong request method"]);
 }
