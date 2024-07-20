79019bdcb93448fd22a3242d798f4439e2feb7da
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-07-21 00:16:06 +0300

    adding delete method

--- restaurants/create.php
+++ restaurants/create.php
@@ -5,13 +5,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $location = $_POST["location"];
     $cuisine_type = $_POST["cuisine_type"];
-    // $id = $_POST["id"];
+
 if ($name != "" && $location != "" && $cuisine_type != "") {
     $stmt = $conn->prepare("select * from resturants where name=?");
     $stmt->bind_param("s", $name);
     $stmt->execute();
     $result = $stmt->get_result();
-    // print_r($result);
+
     if ($result->num_rows == 0) {
         $stmt = $conn->prepare("insert into resturants (id, name, location, cuisine_type) values(?,?,?,?)");
         $stmt->bind_param("isss", $id, $name, $location, $cuisine_type);
